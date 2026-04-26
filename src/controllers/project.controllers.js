b2a0dd0e3c57e088f8318aeb4efe14a21eb10edd
Arnav Alok <arnavaalok@gmail.com>
2026-04-26 20:22:32 +0530

    Implements project listing with member and role details
    Enables fetching all projects for the authenticated user, including
    project metadata, member count, and the user's role in each project.
    Improves data retrieval efficiency and supports enhanced frontend
    display of user-specific project information.

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -6,7 +6,60 @@ import { Project } from "../models/project.models.js";
 import { ProjectMember } from "../models/projectmembers.model.js";
 import mongoose from "mongoose";
 
-const getProjects = asyncHandler(async (req, res) => {});
+const getProjects = asyncHandler(async (req, res) => {
+    const projects = await ProjectMember.aggregate([
+        {
+            $match: {
+                user: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "projects",
+                localField: "project",
+                foreignField: "_id",
+                as: "projects",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "projectmembers",
+                            localField: "_id",
+                            foreignField: "projects",
+                            as: "projectmembers"
+                        }
+                    },
+                    {
+                        $addFields:{
+                            members: {
+                                $size: "$projectmembers"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$projects"
+        },
+        {
+            $project: {
+                project: {
+                    _id: 1,
+                    name: 1,
+                    description: 1,
+                    members: 1,
+                    createdAt: 1,
+                    createdBy: 1
+                },
+                role: 1, 
+                _id: 0,
+
+            }
+        }
+    ]
+    )
+    return res.status(200).json(new ApiResponse(200, projects, "Projects fetched successfully"))
+});
 
 const getProjectById = asyncHandler(async (req, res) => {});
 
