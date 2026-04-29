2934428e1a6330ca2af931483a32d9a86397ca7a
Arnav Alok <arnavaalok@gmail.com>
2026-04-29 21:13:49 +0530

    Adds project role-based permission middleware
    Introduces middleware to validate user roles for project access and refactors JWT verification for improved error handling and consistency.
    Enables scalable authorization checks based on project membership and assigned roles.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,27 +1,60 @@
-import {User} from "../models/user.models.js"
-import { ApiError } from "../utils/api-error.js"
-import { asyncHandler } from "../utils/async-handler.js"
-import jwt from "jsonwebtoken"
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import jwt from "jsonwebtoken";
+import { ProjectMember } from "../models/projectmembers.model.js";
 
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
 
+  if (!token) {
+    throw new ApiError(401, "Unauthorized request");
+  }
 
-export const verifyJWT = asyncHandler(async(req, res, next) =>{
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+    );
 
-    if (!token){
-        throw new ApiError(401, "Unauthorized request")
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid Access Token");
+  }
+});
+
+export const validateProjectPermission = (roles = []) => {
+  asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw new ApiError(400, "Project ID is missing");
     }
 
-    try {
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry")
+    const project = await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user._id),
+    });
 
-        if (!user){
-            throw new ApiError(401, "Invalid Access Token")
-        }
-        req.user = user
-        next()
-    } catch (error) {
-        throw new ApiError(401, "Invalid Access Token")
+    if (!project) {
+      throw new ApiError(400, "Project not found");
     }
-})
+
+    const givenRole = project?.role;
+
+    req.user.role = givenRole;
+
+    if (!roles.includes(givenRole)) {
+      throw new ApiError(
+        403,
+        "You do not have permission to perform this action",
+      );
+    }
+  });
+};
