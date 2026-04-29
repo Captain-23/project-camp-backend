79da3780909f8ee9ab860e99899afec493d9ce57
Arnav Alok <arnavaalok@gmail.com>
2026-04-29 21:21:01 +0530

    Adds project and member validators, refactors code style
    Introduces validation for project creation and adding project members,
    including role checks and improved error messages. Refactors code for
    consistency and readability. Enhances input validation coverage for new
    project-related features.

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,73 +1,83 @@
-import {body} from "express-validator";
-
+import { body } from "express-validator";
+import { AvailableUserRole } from "../utils/constants.js";
 
 const userRegisterValidator = () => {
-    return [
-        body("email")
-            .trim()
-            .notEmpty()
-            .withMessage("Email is required")
-            .isEmail()
-            .withMessage("Email is invalid"),
-        body("username")
-            .trim()
-            .notEmpty()
-            .withMessage("Username is required")
-            .isLowercase()
-            .withMessage("Username must be in lowercase")
-            .isLength({min: 3})
-            .withMessage("Username must be 3 characters long"),
-        body("password")
-            .trim()
-            .notEmpty()
-            .withMessage("Password can't be empty"),
-        body("fullName")
-            .optional()
-            .trim()
-    ]
-}
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase()
+      .withMessage("Username must be in lowercase")
+      .isLength({ min: 3 })
+      .withMessage("Username must be 3 characters long"),
+    body("password").trim().notEmpty().withMessage("Password can't be empty"),
+    body("fullName").optional().trim(),
+  ];
+};
 
 const userLoginValidator = () => {
-      return [
-        body("email")
-        .optional()
-        .isEmail()
-        .withMessage("Email is invalid"),
-        body("password")
-            .notEmpty()
-            .withMessage("Password is required")
+  return [
+    body("email").optional().isEmail().withMessage("Email is invalid"),
+    body("password").notEmpty().withMessage("Password is required"),
+  ];
+};
 
-      ]
-}
-
-const userChangeCurrentPasswordValidator = () =>{
-    return [
-        body("oldPassword").notEmpty().withMessage("Old password is required"),
-        body("newPassword").notEmpty().withMessage("New password is required")
-    ]
-}
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword").notEmpty().withMessage("Old password is required"),
+    body("newPassword").notEmpty().withMessage("New password is required"),
+  ];
+};
 
 const userforgotCurrentPasswordValidator = () => {
-    return [
-        body("email")
-            .notEmpty()
-            .withMessage("Email is required")
-            .isEmail()
-            .withMessage("Email is invalid")
-    ]
-}
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+  ];
+};
 
-const userResetCurrentPasswordValidator = ()=> {
-    return [
-        body("newPassword")
-            .notEmpty()
-            .withMessage("Password is required")
-    ]
-}
+const userResetCurrentPasswordValidator = () => {
+  return [body("newPassword").notEmpty().withMessage("Password is required")];
+};
+
+const createProjectValidator = () => {
+  return [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("description").optional(),
+  ];
+};
+
+const addMembertoProjectValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("role")
+      .notEmpty()
+      .withMessage("Role is required")
+      .isIn(AvailableUserRole)
+      .withMessage("Role is invalid"),
+  ];
+};
 export {
-    userRegisterValidator,
-    userLoginValidator,
-    userChangeCurrentPasswordValidator,
-    userforgotCurrentPasswordValidator,
-    userResetCurrentPasswordValidator,
-}
+  userRegisterValidator,
+  userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userforgotCurrentPasswordValidator,
+  userResetCurrentPasswordValidator,
+  createProjectValidator,
+  addMembertoProjectValidator,
+};
